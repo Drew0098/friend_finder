32b4db32850db8d31fc6ec00cfc35376ff44326d
Drew0098 <a.eaton0098@gmail.com>
2018-02-08 15:52:31 -0500

    first and final commit. Please get on me more for doing this.

---
+++ app/data/friends.js
@@ -0,0 +1,102 @@
+var friends = [
+    {
+        "name": "Ahmed Nazeri",
+        "photo": "https://www.mubadala.com/sites/default/files/leadership/Ahmed-Yahia-Al-Idrissi-resized_1.png",
+        "scores": [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    },
+    {
+        "name": "Jacob Smith",
+        "photo": "https://s3media.247sports.com/Uploads/Assets/912/529/6_5529912.jpg",
+        "scores": [
+            "4",
+            "2",
+            "5",
+            "1",
+            "3",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2"
+        ]
+    },
+    {
+        "name": "Emily Johnson",
+        "photo": "http://www.alltheinfo.org/wp-content/uploads/2011/04/maine-usa-2011-emily-johnson-010.jpg",
+        "scores": [
+            "5",
+            "2",
+            "2",
+            "2",
+            "4",
+            "1",
+            "3",
+            "2",
+            "5",
+            "5"
+        ]
+    },
+    {
+        "name": "Bobby Brown",
+        "photo": "http://media.tmz.com/2015/04/27/bobby-brown-200x250.jpg",
+        "scores": [
+            "3",
+            "3",
+            "4",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2",
+            "2",
+            "3"
+        ]
+    },
+    {
+        "name": "Peter Wiggin",
+        "photo": "http://www.enderwiggin.net/wp-content/uploads/2013/02/Jimmy-Jax-447x550.jpg",
+        "scores": [
+            "4",
+            "3",
+            "4",
+            "1",
+            "5",
+            "2",
+            "5",
+            "3",
+            "1",
+            "4"
+        ]
+    },
+    {
+        "name": "Emmerson Moore",
+        "photo": "https://lh4.googleusercontent.com/-33HnIc44tZ8/AAAAAAAAAAI/AAAAAAAAAD8/TFxYu1ZZTmU/photo.jpg",
+        "scores": [
+            "4",
+            "4",
+            "2",
+            "3",
+            "2",
+            "2",
+            "3",
+            "2",
+            "4",
+            "5"
+        ]
+    }
+
+]
+
+
+module.exports = friends;
